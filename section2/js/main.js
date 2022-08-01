e8779a313c758b815d2d2ce4aef4222d39f73b83
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-08-01 12:17:41 +0900

    Loading external data file

--- section2/js/main.js
+++ section2/js/main.js
@@ -1,23 +1,36 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    2.6 - Adding SVGs with D3
+ *    2.7 - Adding SVGs with D3
  */
 
-const data = [25, 20, 10, 12, 15];
+d3.csv('data/ages.csv')
+  .then(data => {
+    data.forEach(d => {
+      d.age = Number(d.age);
+    });
+    const svg = d3
+      .select('#chart-area')
+      .append('svg')
+      .attr('width', 400)
+      .attr('height', 400);
 
-const svg = d3
-  .select('#chart-area')
-  .append('svg')
-  .attr('width', 400)
-  .attr('height', 400);
+    const circles = svg.selectAll('circle').data(data);
 
-const circles = svg.selectAll('circle').data(data);
-
-circles
-  .enter()
-  .append('circle')
-  .attr('cx', (d, i) => i * 50 + 50)
-  .attr('cy', 250)
-  .attr('r', d => d)
-  .attr('fill', 'red');
+    circles
+      .enter()
+      .append('circle')
+      .attr('cx', (d, i) => i * 50 + 50)
+      .attr('cy', 250)
+      .attr('r', d => 2 * d.age)
+      .attr('fill', d => {
+        if (d.name === 'Tony') {
+          return 'blue';
+        } else {
+          return 'red';
+        }
+      });
+  })
+  .catch(error => {
+    console.log(error);
+  });

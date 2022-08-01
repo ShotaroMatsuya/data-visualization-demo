6e45d6758eb43a3b40984221b798709ce732a81a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-08-01 12:29:28 +0900

    add barchart

--- section2/js/main.js
+++ section2/js/main.js
@@ -1,7 +1,7 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    2.7 - Adding SVGs with D3
+ *    2.8 - Adding SVGs with D3
  */
 
 d3.csv('data/ages.csv')
@@ -34,3 +34,26 @@ d3.csv('data/ages.csv')
   .catch(error => {
     console.log(error);
   });
+
+// homework
+d3.json('data/buildings.json').then(data => {
+  data.forEach(d => {
+    d.height = Number(d.height);
+  });
+  const svg = d3
+    .select('#chart-area2')
+    .append('svg')
+    .attr('width', 500)
+    .attr('height', 500);
+
+  const rects = svg.selectAll('rect').data(data);
+
+  rects
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => i * 50 + 50)
+    .attr('y', 5)
+    .attr('width', 40)
+    .attr('height', d => d.height)
+    .attr('fill', 'gray');
+});

6283995eb4aba3c4a4ef588164b60929df247938
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-08-01 15:10:44 +0900

    Making a bar chart

--- section3/js/main.js
+++ section3/js/main.js
@@ -1,7 +1,7 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    3.10 - Axes and labels
+ *    3.11 - Making a bar chart
  */
 
 const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 };
@@ -52,7 +52,7 @@ d3.json('data/buildings.json').then(data => {
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.height)])
-    .range([0, HEIGHT]);
+    .range([HEIGHT, 0]); // y座標は下がプラスになるのでrangeを逆転させる
 
   const xAxisCall = d3.axisBottom(x);
   g.append('g')
@@ -76,9 +76,9 @@ d3.json('data/buildings.json').then(data => {
   rects
     .enter()
     .append('rect')
-    .attr('y', 0)
+    .attr('y', d => y(d.height)) //上のスペース
     .attr('x', d => x(d.name))
     .attr('width', x.bandwidth)
-    .attr('height', d => y(d.height))
+    .attr('height', d => HEIGHT - y(d.height)) // バーの長さ
     .attr('fill', 'grey');
 });

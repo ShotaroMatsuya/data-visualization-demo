44f89b4499692d15fb5839f1e44bbec99258b6cb
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-08-01 14:55:10 +0900

    Margin and Group

--- section3/js/main.js
+++ section3/js/main.js
@@ -1,14 +1,22 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    3.7 - D3 min, max, and extent
+ *    3.9 - Margins and groups
  */
+
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
 
 const svg = d3
   .select('#chart-area')
   .append('svg')
-  .attr('width', 400)
-  .attr('height', 400);
+  .attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+const g = svg
+  .append('g')
+  .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
 d3.json('data/buildings.json').then(data => {
   data.forEach(d => {
@@ -18,16 +26,16 @@ d3.json('data/buildings.json').then(data => {
   const x = d3
     .scaleBand()
     .domain(data.map(d => d.name))
-    .range([0, 400])
+    .range([0, WIDTH])
     .paddingInner(0.3)
     .paddingOuter(0.2);
 
   const y = d3
     .scaleLinear()
     .domain([0, d3.max(data, d => d.height)])
-    .range([0, 400]);
+    .range([0, HEIGHT]);
 
-  const rects = svg.selectAll('rect').data(data);
+  const rects = g.selectAll('rect').data(data);
 
   rects
     .enter()

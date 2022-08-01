a4651c1d061f2a300c072ede38b8e98a1b87c999
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-08-01 14:50:57 +0900

    Bnad Scales

--- section3/js/main.js
+++ section3/js/main.js
@@ -1,7 +1,7 @@
 /*
  *    main.js
  *    Mastering Data Visualization with D3.js
- *    3.2 - Linear scales
+ *    3.6 - Band scales
  */
 
 const svg = d3
@@ -15,6 +15,21 @@ d3.json('data/buildings.json').then(data => {
     d.height = Number(d.height);
   });
 
+  const x = d3
+    .scaleBand()
+    .domain([
+      'Burj Khalifa',
+      'Shanghai Tower',
+      'Abraj Al-Bait Clock Tower',
+      'Ping An Finance Centre',
+      'Lotte World Tower',
+      'One World Trade Center',
+      'CTF Finance Centre',
+    ])
+    .range([0, 400])
+    .paddingInner(0.3)
+    .paddingOuter(0.2);
+
   const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
 
   const rects = svg.selectAll('rect').data(data);
@@ -23,8 +38,8 @@ d3.json('data/buildings.json').then(data => {
     .enter()
     .append('rect')
     .attr('y', 0)
-    .attr('x', (d, i) => i * 60)
-    .attr('width', 40)
+    .attr('x', d => x(d.name))
+    .attr('width', x.bandwidth)
     .attr('height', d => y(d.height))
     .attr('fill', 'grey');
 });
